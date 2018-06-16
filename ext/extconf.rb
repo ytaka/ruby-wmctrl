dbb4e18cae58deb05537edc6fb641a94cb851731
epitron <chris@ill-logic.com>
2018-06-16 18:09:58 -0400

    Fixed an extconf.rb bug (the library wasn't getting linked against libXmu)

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -8,6 +8,7 @@ $CFLAGS << " " << glib.cflags
 $libs << " " << glib.libs
 
 dir_config("X11")
-if have_header('X11/Xlib.h') && have_library('X11')
+if have_header('X11/Xlib.h') && have_library('X11') && 
+   have_header('X11/Xmu/WinUtil.h') && have_library('Xmu')
   create_makefile("wmctrl")
 end
--- lib/wmctrl/version.rb
+++ lib/wmctrl/version.rb
@@ -1,3 +1,3 @@
 class WMCtrl
-  VERSION = "0.0.6"
+  VERSION = "0.0.7"
 end
